86090b2dd3746c91df0b2593e4f228e19b9114ee
ekworsham <145511698+ekworsham@users.noreply.github.com>
2025-12-02 19:54:23 -0500

    Add missing tags for Locations and Participants in Swagger documentation

--- swagger.js
+++ swagger.js
@@ -22,6 +22,14 @@ const doc = {
     {
       name: 'Food',
       description: 'Endpoints for managing dishes/food'
+    },
+    {
+      name: 'Locations',
+      description: 'Endpoints for managing locations'
+    },
+    {
+      name: 'Participants',
+      description: 'Endpoints for managing participants'
     }
   ]
 };
