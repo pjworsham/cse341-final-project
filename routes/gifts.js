a289271567e67726c236fcc5cba529aa2b770cea
vsyang <yang.vanessa85@gmail.com>
2025-12-02 20:01:50 -0700

    updated oath for both locations and participants

--- routes/gifts.js
+++ routes/gifts.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const giftsController = require('../controllers/gifts');
-
 const {isAuthenticated} = require('../middleware/authenticate');
 const validation = require('../middleware/validate');
 
